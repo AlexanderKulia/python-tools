bd1a369ec835121110ece4eb8bef7e66ca55a69b
AlexanderKulia <kaaprivateproperty@gmail.com>
2024-03-08 23:50:08 +0100

    feat: add common files logic

--- python_tools/main.py
+++ python_tools/main.py
@@ -18,13 +18,24 @@ class FileCounter:
 
 
 @dataclass
-class Counter:
+class ComponentCounter:
+    component_path: Path
     statement_count: int
     file_counters: list[FileCounter]
 
 
-def generate_file_stats(folder_path: Path) -> Counter:
-    file_counters: list[FileCounter] = []
+@dataclass
+class GlobalCounter:
+    statement_count: int
+    component_counters: list[ComponentCounter]
+
+
+def generate_stats(folder_path: Path) -> GlobalCounter:
+    component_counters: dict[Path, ComponentCounter] = defaultdict(
+        lambda: ComponentCounter(
+            component_path=Path(), statement_count=0, file_counters=[]
+        )
+    )
     total_statement_count = 0
 
     for dir_path, dir_names, file_names in folder_path.walk():
@@ -56,15 +67,20 @@ def generate_file_stats(folder_path: Path) -> Counter:
                         for name in node.names:
                             imports.add(name.name)
 
-            file_counters.append(
-                FileCounter(
-                    file_path=file_path,
-                    statement_count=file_statement_count,
-                    imports=imports,
-                )
+            file_counter = FileCounter(
+                file_path=file_path,
+                statement_count=file_statement_count,
+                imports=imports,
             )
+            component_path = file_path.parent
+            component_counters[component_path].component_path = component_path
+            component_counters[component_path].statement_count += file_statement_count
+            component_counters[component_path].file_counters.append(file_counter)
 
-    return Counter(statement_count=total_statement_count, file_counters=file_counters)
+    return GlobalCounter(
+        statement_count=total_statement_count,
+        component_counters=list(component_counters.values()),
+    )
 
 
 def is_statement(node: ast.AST) -> bool:
@@ -103,64 +119,81 @@ def should_skip_file(file_name: str) -> bool:
 
 if __name__ == "__main__":
     root_path = Path(".").resolve()
-    stats = generate_file_stats(root_path)
+    stats = generate_stats(root_path)
     is_success = True
     max_allowed_statement_percent_per_file = 0.1
     max_allowed_statement_count_per_file = round(
         stats.statement_count * max_allowed_statement_percent_per_file
     )
 
-    component_map: dict[str, int] = defaultdict(int)
+    for component_stat in stats.component_counters:
+        for file_stat in component_stat.file_counters:
+            for import_ in file_stat.imports:
+                if "/libs/" in str(file_stat.file_path) and "packages" in import_:
+                    logger.warning(
+                        f"Path {file_stat.file_path} should not import {import_}"
+                    )
+                    is_success = False
 
-    for stat in stats.file_counters:
-        component_name = stat.file_path.parent
-        component_map[str(component_name)] += stat.statement_count
-
-        for import_ in stat.imports:
-            if "/libs/" in str(stat.file_path) and "packages" in import_:
-                logger.warning(f"Path {stat.file_path} should not import {import_}")
-                is_success = False
-
-        if stat.statement_count > max_allowed_statement_count_per_file:
-            logger.warning(
-                f"Path {stat.file_path} has {round(stat.statement_count)} statements. Maximim allowed statement count per file is {max_allowed_statement_count_per_file}"
-            )
+            if file_stat.statement_count > max_allowed_statement_count_per_file:
+                logger.warning(
+                    f"Path {file_stat.file_path} has {round(file_stat.statement_count)} statements. Maximim allowed statement count per file is {max_allowed_statement_count_per_file}"
+                )
 
     square_diff_sum = 0
-    component_count = len(component_map.keys())
+    component_count = len(stats.component_counters)
     component_statement_mean = stats.statement_count / component_count
 
-    for component_path, statement_count in component_map.items():
-        diff = statement_count - component_statement_mean
+    for component_stat in stats.component_counters:
+        diff = abs(component_stat.statement_count - component_statement_mean)
         square_diff_sum += diff**2
-        diff_from_mean = abs(statement_count - component_statement_mean)
 
     std_dev = (square_diff_sum / (component_count - 1)) ** 0.5
 
     leaf_nodes: set[str] = set()
     common_components: set[str] = set()
 
-    for component_path, statement_count in component_map.items():
-        diff_from_mean = abs(statement_count - component_statement_mean)
+    for component_stat in stats.component_counters:
+        diff_from_mean = abs(component_stat.statement_count - component_statement_mean)
         std_dev_count = diff_from_mean / std_dev
 
         if std_dev_count > 3:
             logger.warning(
-                f"Path {stat.file_path} is an outlier with {std_dev_count} standard deviations from the mean"
+                f"Path {component_stat} is an outlier with {std_dev_count} standard deviations from the mean"
             )
             is_success = False
 
-        leaf_name = Path(component_path).name
+        leaf_name = Path(component_stat.component_path).name
         if leaf_name in leaf_nodes:
-            common_components.add(component_path)
+            common_components.add(str(component_stat.component_path))
         else:
             leaf_nodes.add(leaf_name)
 
     if len(common_components) > 0:
         logger.warning(f"There are common component names {common_components}")
 
+    seen_files: set[str] = set()
+    common_files: set[str] = set()
+
+    for component_stat in stats.component_counters:
+        for file_stat in component_stat.file_counters:
+            file_name = file_stat.file_path.name
+            if file_name in seen_files:
+                common_files.add(file_name)
+            else:
+                seen_files.add(file_name)
+
+    if len(common_files) > 0:
+        logger.warning(f"There are common file names {common_files}")
+
     with open("components.json", "w") as f:
-        json.dump(component_map, f)
+        json.dump(
+            {
+                str(stat.component_path): stat.statement_count
+                for stat in stats.component_counters
+            },
+            f,
+        )
 
     if not is_success:
         sys.exit(1)
